99e6a194ebd72453e47747e3d33f4b2226e05d04
merkur <merkur@P00071974-001.CNS.LGCNS.COM>
2020-03-23 18:32:42 +0900

    first commit

---
+++ Chapter4/src/section2/Polynomial.java
@@ -0,0 +1,73 @@
+package section2;
+
+import section1.MySingleLinkedList;
+import section1.Node;
+
+public class Polynomial {
+	public String name;
+	public MySingleLinkedList<Term> terms;
+
+	public Polynomial(String name) {
+		this.name = name;
+		terms = new MySingleLinkedList<Term>();
+	}
+	
+	public void addTerm(int coef, int expo) {
+		if(coef == 0)
+			return;
+		
+		Node<Term> p = terms.head;
+		Node<Term> before = null;
+		
+		while(p != null && p.data.expo > expo) {
+			before = p;
+			p = p.next;
+		}
+		
+		if(p != null && p.data.expo == expo) {	//동일한 항이 존재
+			p.data.coef += coef;
+			if(p.data.coef == 0) {
+				if(before == null) {
+					terms.removeFirst();
+				} else {
+					terms.removeAfter(before);
+				}
+			}
+		} else {//신규항 추가
+			Term term = new Term(coef, expo);
+			if(before == null)
+				terms.addFirst(term);
+			else
+				terms.addAfter(before, term);
+		}
+	}
+
+	public int calc(int x) {
+		int result = 0;
+		Node<Term> p = terms.head;
+		
+		while(p != null) {
+			result += p.data.calc(x);
+			p = p.next;
+		}
+		
+		return result;
+	}
+
+	@Override
+	public String toString() { 
+		StringBuilder sb = new StringBuilder();
+		sb.append(name + "(x)=");
+		Node<Term> p = terms.head;
+		
+		while(p != null) {
+			sb.append(" + " + p.data.toString());
+			p = p.next;
+		}
+		return sb.toString();
+	}
+
+	public static void main(String[] args) {
+		
+	}
+}
